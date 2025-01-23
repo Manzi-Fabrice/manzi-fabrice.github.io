16744af1658bad53c1e41d7bfcd264924af636c8
Manzi Fabrice Niyigaba <manzi.fabrice.niyigaba.27@dartmouth.edu.com>
2025-01-22 21:10:06 -0500

    added necessary files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("starting up");
+import $ from 'jquery';
+import './style.scss';
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let days = 0;
+
+$('#main').html(`You Have been to this page for ${seconds} seconds.`);
+
+const updateDisplay = () => {
+  $('#main').html(
+    `You Have been to this page for: ${days} days: ${hours} hours: ${minutes} minutes:${seconds} seconds.`,
+  );
+};
+
+updateDisplay();
+
+setInterval(() => {
+  seconds += 1;
+
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes += 1;
+  }
+
+  if (minutes >= 60) {
+    minutes = 0;
+    hours += 1;
+  }
+
+  if (hours >= 24) {
+    hours = 0;
+    days += 1;
+  }
+
+  updateDisplay();
+}, 1000);
