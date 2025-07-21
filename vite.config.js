6cec5b5fdaa1099882ab0671035b22aa4f408ee7
Manzi Fabrice Niyigaba <manzi.fabrice.niyigaba.27@dartmouth.edu.com>
2025-07-20 22:56:39 -0400

    added some deployment packages

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import eslint from "vite-plugin-eslint";
 import autoprefixer from 'autoprefixer';
 import faviconPlugin from "vite-plugin-favicon";
 
-
-
 export default defineConfig({
+  base: '/',  // <-- Add this line
   plugins: [eslint(), faviconPlugin('public/favicon.png')],
   css: {
     postcss: {
-      plugins: [
-        autoprefixer(),
-      ],
+      plugins: [autoprefixer()],
     },
   },
 });
